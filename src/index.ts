0fb6dbf419a0f46b02c9e6d9cc369d0539b6c934
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-03 18:29:39 +0700

    feat: Create container that sleep

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,23 @@ import Docker from 'dockerode'
 
 const docker = new Docker()
 
-const images = await docker.listImages()
+console.log('pull start')
 
-console.log(images)
+const pullStream = await docker.pull('ubuntu:22.04')
+
+await new Promise((resolve, reject) => {
+	docker.modem.followProgress(pullStream, (err, res) =>
+		err ? reject(err) : resolve(res)
+	)
+})
+
+console.log('pull end')
+
+// await docker.run('ubuntu:22.04', ['/bin/bash'], process.stdout, {}, {})
+
+const container = await docker.createContainer({
+	Image: 'ubuntu:20.04',
+	Cmd: ['sleep', 'infinity'],
+})
+
+await container.start()
