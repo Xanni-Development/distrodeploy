105a323eb331c58c0fd179f8fa2ea5796656bef1
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-04 11:09:33 +0700

    feat: Docker support os param

--- src/Providers/Docker/DockerProvider.ts
+++ src/Providers/Docker/DockerProvider.ts
@@ -11,7 +11,15 @@ class DockerProvider extends Provider {
 		os: OperatingSystem,
 		options: ICreateVMOptions
 	): Promise<VM> {
-		const pullStream = await this.docker.pull('ubuntu:22.04')
+		const imageName = this.getDockerImageNameFromOS(os)
+
+		if (imageName === null) {
+			throw new Error(
+				`Docker provider doesn't support os ${OperatingSystem[os]}`
+			)
+		}
+
+		const pullStream = await this.docker.pull(imageName)
 
 		await new Promise((resolve, reject) => {
 			this.docker.modem.followProgress(pullStream, (err, res) =>
@@ -21,7 +29,7 @@ class DockerProvider extends Provider {
 
 		// https://gdevillele.github.io/engine/reference/api/docker_remote_api_v1.24/#/create-a-container
 		const container = await this.docker.createContainer({
-			Image: 'ubuntu:20.04',
+			Image: imageName,
 			Cmd: ['sleep', 'infinity'],
 			HostConfig: {
 				Memory: options.memory,
@@ -35,6 +43,15 @@ class DockerProvider extends Provider {
 
 	async getByID(id: string): Promise<VM> {
 		return new DockerVM(this.docker.getContainer(id))
+	}
+
+	private getDockerImageNameFromOS(os: OperatingSystem): string | null {
+		switch (os) {
+			case OperatingSystem['Ubuntu:20.04']:
+				return 'ubuntu:22.04'
+			default:
+				return null
+		}
 	}
 }
 
