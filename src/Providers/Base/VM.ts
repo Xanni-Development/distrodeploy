c4d7815195319a1763940eda43eb80d10a05a600
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-04 10:44:01 +0700

    feat: Abstract create options getter

--- src/Providers/Base/VM.ts
+++ src/Providers/Base/VM.ts
@@ -1,7 +1,10 @@
+import { ICreateVMOptions } from './Provider'
 import Shell from './Shell'
 
 abstract class VM {
 	abstract createShell(): Promise<Shell>
+
+	abstract get createOptions(): ICreateVMOptions
 }
 
 export default VM
