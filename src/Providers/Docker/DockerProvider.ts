8e46e80b350032ffa3feda87e66472464af4b96e
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-04 10:41:06 +0700

    feat: Docker provider create vm options

--- src/Providers/Docker/DockerProvider.ts
+++ src/Providers/Docker/DockerProvider.ts
@@ -1,13 +1,16 @@
 import Dockerode from 'dockerode'
 import OperatingSystem from '../../OperatingSystem'
-import Provider from '../Base/Provider'
+import Provider, { ICreateVMOptions } from '../Base/Provider'
 import VM from '../Base/VM'
 import DockerVM from './DockerVM'
 
 class DockerProvider extends Provider {
 	private docker = new Dockerode()
 
-	async createVM(os: OperatingSystem): Promise<VM> {
+	async createVM(
+		os: OperatingSystem,
+		options: ICreateVMOptions
+	): Promise<VM> {
 		const pullStream = await this.docker.pull('ubuntu:22.04')
 
 		await new Promise((resolve, reject) => {
@@ -16,14 +19,20 @@ class DockerProvider extends Provider {
 			)
 		})
 
+		// https://gdevillele.github.io/engine/reference/api/docker_remote_api_v1.24/#/create-a-container
 		const container = await this.docker.createContainer({
 			Image: 'ubuntu:20.04',
 			Cmd: ['sleep', 'infinity'],
+			HostConfig: {
+				Memory: options.memory,
+				CpuPeriod: Math.floor(options.cpus) * 100000,
+				CpuQuota: options.cpus * 100000,
+			},
 		})
 
 		await container.start()
 
-		return new DockerVM(container)
+		return new DockerVM(container, options)
 	}
 }
 
--- src/Providers/Docker/DockerVM.ts
+++ src/Providers/Docker/DockerVM.ts
@@ -2,14 +2,20 @@ import Dockerode from 'dockerode'
 import VM from '../Base/VM'
 import Shell from '../Base/Shell'
 import DockerShell from './DockerShell'
+import { ICreateVMOptions } from '../Base/Provider'
 
 class DockerVM extends VM {
 	private container: Dockerode.Container
+	private createOptions: ICreateVMOptions
 
-	constructor(container: Dockerode.Container) {
+	constructor(
+		container: Dockerode.Container,
+		createOptions: ICreateVMOptions
+	) {
 		super()
 
 		this.container = container
+		this.createOptions = createOptions
 	}
 
 	async createShell(): Promise<Shell> {
