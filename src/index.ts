2bac3a4834f1daa266c4324e8f7736146aa196b1
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-04 13:44:29 +0700

    feat: Test

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import DockerProvider from './Providers/Docker/DockerProvider'
 
 const provider = new DockerProvider()
 
-const vm = await provider.createVM(OperatingSystem['Ubuntu:20.04'], {
+const vm = await provider.createVM(OperatingSystem['Ubuntu:22.04'], {
 	cpus: 1,
 	memory: 100 * 1024 * 1024, //100MB
 })
@@ -16,4 +16,11 @@ const shell = await vm.createShell()
 
 shell.getStdoutStream().pipe(process.stdout)
 
+shell.write('echo "hi lol" > test.txt\n')
+shell.write('pwd\n')
 shell.write('ls\n')
+// shell.write('ls bin\n')
+shell.write('cat test.txt\n')
+// shell.write('apt update\n')
+// shell.write('apt install nano\n')
+shell.write('nano test.txt\n')
