e84cbb02f7a81feeb7bb3ff88d99a7790d4e9492
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-05 19:23:32 +0700

    feat: Stop vm command

---
+++ src/DiscordBot/Commands/StopVM.ts
@@ -0,0 +1,53 @@
+import { SlashCommandBuilder } from 'discord.js'
+import { ICommand } from './Types'
+import prisma from '../Database'
+import BotProvider from '../Data/BotProvider'
+import { ContainerState } from '../../Providers/Base/VM'
+
+const StopVM: ICommand = {
+	data: new SlashCommandBuilder()
+		.setName('stop-vm')
+		.setDescription('Stop selected vm!')
+		.addNumberOption(option =>
+			option
+				.setName('seconds-wait-before-kill-vm')
+				.setDescription('How many seconds to wait before kill VM')
+				.setRequired(false)
+		) as SlashCommandBuilder,
+	async execute(interaction) {
+		await interaction.deferReply()
+
+		const secondsWaitBeforeKillVM =
+			interaction.options.getNumber('seconds-wait-before-kill-vm', false) ?? 5
+
+		const user = await prisma.user.findFirst({
+			where: { discordID: interaction.user.id },
+			select: { id: true, selectedVM: true },
+		})
+
+		if (user === null)
+			return void (await interaction.editReply(
+				`You need to create an account to use this.`
+			))
+
+		if (user.selectedVM === null)
+			return void (await interaction.editReply(
+				`Cannot find VM with id ${user.selectedVM.id}.`
+			))
+
+		const providerVM = await BotProvider.getVMByID(user.selectedVM.vmID)
+
+		if ((await providerVM.state) !== ContainerState.Running)
+			return void (await interaction.editReply(
+				`Cannot stop VM with id ${user.selectedVM.id} as it's not running.`
+			))
+
+		await providerVM.stop(secondsWaitBeforeKillVM)
+
+		await interaction.editReply(
+			`VM with id ${user.selectedVM.id} succesfully stopped.`
+		)
+	},
+}
+
+export default StopVM
--- src/DiscordBot/Commands/index.ts
+++ src/DiscordBot/Commands/index.ts
@@ -3,7 +3,8 @@ import Hi from './Hi.js'
 import Register from './Register.js'
 import SelectVM from './SelectVM.js'
 import StartVM from './StartVM.js'
+import StopVM from './StopVM.js'
 
-const Commands = [Hi, Register, CreateVM, SelectVM, StartVM]
+const Commands = [Hi, Register, CreateVM, SelectVM, StartVM, StopVM]
 
 export default Commands
