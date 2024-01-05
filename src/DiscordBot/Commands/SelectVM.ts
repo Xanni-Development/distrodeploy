0286b48d803c0365c8567da83d8da4e871e93cc3
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-05 16:55:52 +0700

    feat: Select vm

---
+++ src/DiscordBot/Commands/SelectVM.ts
@@ -0,0 +1,61 @@
+import { SlashCommandBuilder } from 'discord.js'
+import { ICommand } from './Types'
+import prisma from '../Database'
+
+const SelectVM: ICommand = {
+	data: new SlashCommandBuilder()
+		.setName('select-vm')
+		.setDescription('Select a vm!')
+		.addIntegerOption(option =>
+			option
+				.setName('vm-id')
+				.setDescription('Virtual Machine ID')
+				.setRequired(true)
+		) as SlashCommandBuilder,
+	async execute(interaction) {
+		await interaction.deferReply()
+
+		const vmID = interaction.options.getInteger('vm-id')
+
+		const user = await prisma.user.findFirst({
+			where: { discordID: interaction.user.id },
+			select: { id: true },
+		})
+
+		if (user === null)
+			return void (await interaction.editReply(
+				`You need to create an account to use this.`
+			))
+
+		const vm = await prisma.virtualMachine.findFirst({
+			where: {
+				id: vmID,
+				owner: {
+					discordID: interaction.user.id,
+				},
+			},
+		})
+
+		if (vm === null)
+			return void (await interaction.editReply(
+				`Cannot find VM with id ${vmID}. Either it doesn't exist or you're not the owner`
+			))
+
+		await prisma.user.update({
+			where: {
+				id: user.id,
+			},
+			data: {
+				selectedVM: {
+					connect: vm,
+				},
+			},
+		})
+
+		await interaction.editReply(
+			`You have selected vm ${vm.id}`
+		)
+	},
+}
+
+export default SelectVM
--- src/DiscordBot/Commands/index.ts
+++ src/DiscordBot/Commands/index.ts
@@ -1,7 +1,8 @@
 import CreateVM from './CreateVM.js'
 import Hi from './Hi.js'
 import Register from './Register.js'
+import SelectVM from './SelectVM.js'
 
-const Commands = [Hi, Register, CreateVM]
+const Commands = [Hi, Register, CreateVM, SelectVM]
 
 export default Commands
