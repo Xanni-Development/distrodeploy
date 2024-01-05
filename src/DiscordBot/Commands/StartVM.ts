ce89312d9ad6c3500685167351de08e2ff403a81
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-05 18:09:46 +0700

    feat: Start vm command

---
+++ src/DiscordBot/Commands/StartVM.ts
@@ -0,0 +1,44 @@
+import { SlashCommandBuilder } from 'discord.js'
+import { ICommand } from './Types'
+import prisma from '../Database'
+import BotProvider from '../Data/BotProvider'
+
+const StartVM: ICommand = {
+	data: new SlashCommandBuilder()
+		.setName('start-vm')
+		.setDescription('Start selected vm!'),
+	async execute(interaction) {
+		await interaction.deferReply()
+
+		const user = await prisma.user.findFirst({
+			where: { discordID: interaction.user.id },
+			select: { id: true, selectedVM: true },
+		})
+
+		if (user === null)
+			return void (await interaction.editReply(
+				`You need to create an account to use this.`
+			))
+
+		const vm = await prisma.virtualMachine.findFirst({
+			where: {
+				id: user.selectedVM.id,
+			},
+		})
+
+		if (vm === null)
+			return void (await interaction.editReply(
+				`Cannot find VM with id ${user.selectedVM.id}.`
+			))
+
+		const providerVM = await BotProvider.getVMByID(user.selectedVM.vmID)
+
+		await providerVM.start()
+
+		await interaction.editReply(
+			`VM with id ${user.selectedVM.id} succesfully started.`
+		)
+	},
+}
+
+export default StartVM
--- src/DiscordBot/Commands/index.ts
+++ src/DiscordBot/Commands/index.ts
@@ -2,7 +2,8 @@ import CreateVM from './CreateVM.js'
 import Hi from './Hi.js'
 import Register from './Register.js'
 import SelectVM from './SelectVM.js'
+import StartVM from './StartVM.js'
 
-const Commands = [Hi, Register, CreateVM, SelectVM]
+const Commands = [Hi, Register, CreateVM, SelectVM, StartVM]
 
 export default Commands
