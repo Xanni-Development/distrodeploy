9f1981ff49bfeee6716d452c92f1b2cc938b0355
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-05 23:19:26 +0700

    feat: Create shell command

---
+++ src/DiscordBot/Commands/CreateShell.ts
@@ -0,0 +1,60 @@
+import { SlashCommandBuilder } from 'discord.js'
+import { ICommand } from './Types'
+import prisma from '../Database'
+import BotProvider from '../Data/BotProvider'
+import { ContainerState } from '../../Providers/Base/VM'
+
+const CreateShell: ICommand = {
+	data: new SlashCommandBuilder()
+		.setName('create-shell')
+		.setDescription('Create new shell on selected VM!'),
+	async execute(interaction) {
+		await interaction.deferReply()
+
+		const user = await prisma.user.findFirst({
+			where: { discordID: interaction.user.id },
+			select: { id: true, selectedVM: true },
+		})
+
+		if (user === null)
+			return void (await interaction.editReply(
+				`You need to create an account to use this.`
+			))
+
+		if (user.selectedVM === null)
+			return void (await interaction.editReply(
+				`You haven't selected any VM.`
+			))
+
+		const providerVM = await BotProvider.getVMByID(user.selectedVM.vmID)
+
+		if ((await providerVM.state) !== ContainerState.Running)
+			return void (await interaction.editReply(
+				`Cannot create shell in an inactive VM.`
+			))
+
+		const shell = await providerVM.createShell()
+
+		// Need this to get the message for shell
+		const shellMessage = await interaction.channel.send(
+			`Shell will be here. Creating...`
+		)
+
+		const shellResult = await prisma.vMShell.create({
+			data: {
+				shellID: shell.id,
+
+				discordMessageChannelID: shellMessage.channel.id,
+				discordMessageID: shellMessage.id,
+
+				virtualMachine: { connect: user.selectedVM },
+			},
+		})
+
+		await interaction.editReply(
+			`Shell with id ${shellResult.id} in VM ${user.selectedVM.id} succesfully started.`
+		)
+	},
+}
+
+export default CreateShell
--- src/DiscordBot/Commands/index.ts
+++ src/DiscordBot/Commands/index.ts
@@ -1,3 +1,4 @@
+import CreateShell from './CreateShell.js'
 import CreateVM from './CreateVM.js'
 import Hi from './Hi.js'
 import Register from './Register.js'
@@ -5,6 +6,14 @@ import SelectVM from './SelectVM.js'
 import StartVM from './StartVM.js'
 import StopVM from './StopVM.js'
 
-const Commands = [Hi, Register, CreateVM, SelectVM, StartVM, StopVM]
+const Commands = [
+	Hi,
+	Register,
+	CreateVM,
+	SelectVM,
+	StartVM,
+	StopVM,
+	CreateShell,
+]
 
 export default Commands
