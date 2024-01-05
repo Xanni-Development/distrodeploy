8dcdf6105202bd3d9aef73908357b663c782a7b8
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-05 15:31:35 +0700

    chore: Remove sequelize

--- src/DiscordBot/Database/index.ts
+++ src/DiscordBot/Database/index.ts
@@ -1,7 +1,3 @@
-import { Sequelize } from 'sequelize-typescript'
-import User from '../Models/User'
-import VM from '../Models/VM'
-import Shell from '../Models/Shell'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
