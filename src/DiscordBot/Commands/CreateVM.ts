8245d08d93408343e493eda02c5f8bce3016fe1f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-01-05 15:50:19 +0700

    feat: Create vm command

--- src/DiscordBot/Commands/CreateVM.ts
+++ src/DiscordBot/Commands/CreateVM.ts
@@ -1,6 +1,8 @@
 import { SlashCommandBuilder } from 'discord.js'
 import { ICommand } from './Types'
 import OperatingSystem from '../../Providers/OperatingSystem'
+import prisma from '../Database'
+import BotProvider from '../Data/BotProvider'
 
 const CreateVM: ICommand = {
 	data: new SlashCommandBuilder()
@@ -19,6 +21,8 @@ const CreateVM: ICommand = {
 				)
 		) as SlashCommandBuilder,
 	async execute(interaction) {
+		await interaction.deferReply()
+
 		const osString = interaction.options.getString('operating-system')
 
 		if (!Object.keys(OperatingSystem).includes(osString))
@@ -26,7 +30,28 @@ const CreateVM: ICommand = {
 
 		const os = OperatingSystem[osString]
 
-		await interaction.reply(`soon: ${os}`)
+		const user = await prisma.user.findFirst({
+			where: { discordID: interaction.user.id },
+			select: { id: true },
+		})
+
+		const vm = await BotProvider.createVM(os, {
+			cpus: 1,
+			memory: 100 * 1024 * 1024, //100MB
+		})
+
+		await prisma.virtualMachine.create({
+			data: {
+				owner: {
+					connect: user,
+				},
+				vmID: vm.id,
+			},
+		})
+
+		await interaction.editReply(
+			`Your vm with OS ${os} has been created. Select and start it to use it.`
+		)
 	},
 }
 
